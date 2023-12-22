caafb20e622e34c7085d92a4811567a642e0f5b9
sparrow-js <sparrowwht7@gmail.com>
2023-12-22 22:50:31 +0800

    修改提示

--- client/src/components/OnboardingNote.tsx
+++ client/src/components/OnboardingNote.tsx
@@ -1,15 +1,36 @@
 export function OnboardingNote() {
   return (
-    <div className="flex flex-col space-y-4 bg-sky-200 p-2 rounded text-slate-700 text-sm">
-      <span>
-       体验ant-codeAI, 花费预估10元/20次生成
-       <p>
-       (只体验不可超过10元)<span className="text-red-600">不退款</span>
-       </p>
-       <p>email: sparrowwht7@gmail.com</p>
-       <p>wx: sparrow777-js</p>
-       如有openAI API key自行设置使用即可。
-      </span>
-    </div>
+    <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm mx-auto ring-1 ring-slate-900/10">
+        <div className="flex items-baseline space-x-2 mb-6">
+          <span className="text-5xl font-bold">10元/20</span>
+          <span className="text-gray-500">次生成</span>
+        </div>
+        <p className="text-gray-700 mb-6">
+        不退款(初次使用只体验不可超过10元)      
+        </p>
+        <hr className="mb-6"/>
+        <div className="space-y-4">
+          <div className="flex items-center">
+            <i className="fas fa-check-circle text-blue-600 mr-2"></i>
+            <span>
+              email: sparrowwht7@gmail.com
+            </span>
+          </div>
+          <div className="flex items-center">
+            <i className="fas fa-infinity text-blue-600 mr-2"></i>
+            <span>wx: sparrow777-js</span>
+          </div>
+          <div className="flex items-center">
+            <i className="fas fa-users text-blue-600 mr-2"></i>
+            <span>如有openAI API key自行设置使用即可。</span>
+          </div>
+        </div>
+        <div className="flex absolute -bottom-px left-1/2 -ml-36 h-[2px] w-60">
+          <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
+          <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
+          <div className="-ml-[100%] w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
+          <div className="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>
+        </div>
+      </div>
   );
 }

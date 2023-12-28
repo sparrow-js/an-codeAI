354f9a2d69ccc1bc0e70769a492e3189c3c7f872
sparrow-js <sparrowwht7@gmail.com>
2023-12-28 20:34:44 +0800

    fix get access

--- components/components/OnboardingNote.tsx
+++ components/components/OnboardingNote.tsx
@@ -1,3 +1,13 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+
 export default function OnboardingNote() {
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm mx-auto ring-1 ring-slate-900/10">
@@ -5,23 +15,38 @@ export default function OnboardingNote() {
           <span className="text-5xl font-bold">10元/20</span>
           <span className="text-gray-500">次预计生成</span>
         </div>
-        <p className="text-gray-700 mb-6">
-          不退款(初次使用体验不要超过10元),感觉对自己有用在继续使用。每天20:00前发送至邮箱，如有openAI API key自行设置使用即可。
-        </p>
+   
         <hr className="mb-6"/>
         <div className="space-y-4">
-          <div className="">
-            <span>
-              备注一定要添加自己邮箱地址
-            </span>
-            <img className="w-48" src="/2421703471862_.pic.jpg" />
-          </div>
-          <div className="flex items-center">
-            <i className="fas fa-check-circle text-blue-600 mr-2"></i>
-            <span>
-              email: sparrowwht7@gmail.com
-            </span>
-          </div>
+          <Dialog>
+            <DialogTrigger className="rounded-sm w-full">
+              <a href="javascript:void(0);" className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                购买
+              </a>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="mb-4 ">获取 KEY</DialogTitle>
+              </DialogHeader>
+              <div>
+                <p className="text-gray-700 pb-2">
+                不退款(初次使用体验不要超过10元),感觉对自己有用在继续使用。每天20:00前发送Key至邮箱，如有openAI API key自行设置使用即可。
+                </p> 
+                <div className="">
+                  <p className="text-red-500 pb-2">
+                    备注一定要添加自己邮箱地址
+                  </p>
+                  <img className="w-48" src="/2421703471862_.pic.jpg" />
+                </div>
+                <div className="flex items-center">
+                  <i className="fas fa-check-circle text-blue-600 mr-2"></i>
+                  <span>
+                    联系方式 email:  sparrowwht7@gmail.com
+                  </span>
+                </div>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="flex absolute -bottom-px left-1/2 -ml-36 h-[2px] w-60">
           <div className="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"></div>

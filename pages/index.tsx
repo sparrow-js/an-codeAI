f57c645d24c326b5e5351ba66103bdfcfc33cc14
sparrow-js <sparrowwht7@gmail.com>
2024-01-21 20:38:04 +0800

    add Drag & drop svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,7 +124,12 @@ export default function Dashboard() {
                             showAnim={showAnim}
                         />
                     </div>
-                    <p className='mt-8'>Support Drag & drop a screenshot</p>
+                    <p className='mt-8 flex justify-center items-center'>
+                        <span className='mr-2'>
+                        <svg className="icon text-blue-600 w-[35px] h-[35px] fill-current" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5831"><path d="M597.333333 256h85.333334v85.333333h213.333333a42.666667 42.666667 0 0 1 42.666667 42.666667v320L682.666667 554.666667l1.536 343.978666 94.848-91.733333L855.082667 938.666667H384a42.666667 42.666667 0 0 1-42.666667-42.666667v-213.333333H256v-85.333334h85.333333V384a42.666667 42.666667 0 0 1 42.666667-42.666667h213.333333V256z m341.333334 483.754667V896a42.666667 42.666667 0 0 1-2.048 13.098667l-83.626667-144.810667L938.666667 739.754667zM170.666667 597.333333v85.333334H85.333333v-85.333334h85.333334z m0-170.666666v85.333333H85.333333v-85.333333h85.333334z m0-170.666667v85.333333H85.333333V256h85.333334z m0-170.666667v85.333334H85.333333V85.333333h85.333334z m170.666666 0v85.333334H256V85.333333h85.333333z m170.666667 0v85.333334h-85.333333V85.333333h85.333333z m170.666667 0v85.333334h-85.333334V85.333333h85.333334z" p-id="5832"></path></svg>
+                        </span>
+                        Support Drag & drop a screenshot
+                    </p>
                 </div>                    
                 {/* { IS_RUNNING_ON_CLOUD &&
                     !(settings.openAiApiKey) && settings.init && (
